dbbc719db477879321775a2287e970f35d61bd9d
Daniel Knopf <danielkn609@gmail.com>
2021-03-08 18:14:06 -0500

    committed the version of the prelim website with the contact card, text logo, and countdown blob

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+var finalDate = new Date("April 13, 2021, 17:30:00").getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+
+  var distance = finalDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+});
