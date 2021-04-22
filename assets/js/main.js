32a5679fd0b37bfa2a5be09b4e8f26f1a2bb26d1
Daniel Knopf <danielkn609@gmail.com>
2021-04-22 17:50:01 -0400

    changed days to hours

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ var x = setInterval(function () {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
   document.getElementById("minutes").innerHTML = minutes;
   document.getElementById("seconds").innerHTML = seconds;
 });
